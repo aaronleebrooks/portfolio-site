38cfec5f149ecfb4f6dc361c6f3ea68b02e13aba
aaron <pleaseshutupaaron@gmail.com>
2017-11-21 08:37:00 -0500

    fixed jumper buttons, added section headers

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ $(document).ready(function(){
             var newPos = $(document).scrollTop() - 343 ;
             $('#left-holder').css( {top:newPos});
         }
-        
+
         else {
             $('#left-holder').css( {top:0});
         }
     })
 })
+
+$(".jumper").on("click", function( e ) {
+    
+    e.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+    
+});
